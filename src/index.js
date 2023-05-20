8dcdeea605ccbf896673ae2c5fbecc4baf41deb1
theterriblestudent <madondo.sal@gmail.com>
2023-05-20 12:38:57 +0200

    Backing Up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {AppContext} from 'context';
 
 ReactDOM.render(
   <React.StrictMode>
-      <AppContext>
         <App />
-      </AppContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,7 +17,7 @@ function Home () {
             <TopRatedTVSection />
             <PopularMoviesSection />
             <PopularTVShowsSection />
-            {/* <TopBoxOfficeSection /> */}
+            { /* <TopBoxOfficeSection /> */} 
             <PopularPeopleSection />
             <JoinSection />
         </Fragment>
